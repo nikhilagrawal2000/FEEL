2630c4274cc62548123cd9831f3bf0e0e3f94dca
Narendra Patel <patel.ucs2017@iitr.ac.in>
2018-02-05 11:21:39 +0530

    trial

---
+++ index.js
@@ -0,0 +1,55 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    if(firebase.auth().currentUser != null){
+
+      window.location.assign("https://narendrakpatel.github.io/FEEL/pdfselect.html");
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("register_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function register(){
+  document.getElementById("user_div").style.display = "block";
+  document.getElementById("register_div").style.display = "none";
+}
+
+function registeruser(){
+  var email = document.getElementById("email_field").value;
+  var password = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+
+  window.alert("Error : " + errorMessage)
+  // ...
+});
+
+}
